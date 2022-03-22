54ee1002b25cd14e8e2d9fc40e634a8fcd7240a3
Mariluz <90797859+Mariluz18@users.noreply.github.com>
2022-03-22 20:36:53 +0000

    Delete ts directory

--- ts/mail.ts
+++
@@ -1,4 +0,0 @@
-// Creamos la clase MAIL
-export class Mail {
-  constructor(private tipo: string, private direccion: string) {}
-}
--- ts/telefono.ts
+++
@@ -1,4 +0,0 @@
-// Creamos la clase Teléfono
-export class Telefono {
-  constructor(private tipo: string, private numero: string) {}
-}
